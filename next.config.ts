6303c0b3d6e5b746bf1a12f650dfb4eda23551c3
Devendra Desai <devendra.desai@cerner.com>
2024-11-15 14:58:01 +0530

    Working localhost: event 1

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-};
+const nextConfig = {};
 
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,10 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: {
+          dark: "#17131d",
+          light: "#FFF8E8",
+        },
       },
     },
   },
